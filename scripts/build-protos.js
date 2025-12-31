ed00650ea6a8717c32678cab4cb09144e5e96e17
Your Name <you@example.com>
2025-12-31 16:01:03 +0800

    修复：生成并提交 Protobuf bundle.json，修正 pbjs 构建路径

--- scripts/build-protos.js
+++ scripts/build-protos.js
@@ -25,7 +25,8 @@ try {
     console.log('开始转换 Protobuf 文件为 JSON...');
     
     // 使用 npx pbjs 转换为 JSON 格式，这是最通用的格式
-    const command = `npx pbjs -t json -o "${outputFile}" "${protosDir}/common/events.proto" "${protosDir}/common/rpcmeta.proto" "${protosDir}/products/understanding/base/au_base.proto" "${protosDir}/products/understanding/ast/ast_service.proto"`;
+    // 使用 -p 指定 proto 搜索路径，避免 import 时找不到 common/ 下的文件
+    const command = `npx pbjs -t json -p "${protosDir}" -o "${outputFile}" "${protosDir}/common/events.proto" "${protosDir}/common/rpcmeta.proto" "${protosDir}/products/understanding/base/au_base.proto" "${protosDir}/products/understanding/ast/ast_service.proto"`;
     
     console.log('执行命令:', command);
     execSync(command, { stdio: 'inherit' });
